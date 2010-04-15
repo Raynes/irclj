37a96802185bbe5914727411b8ee88c37301433b
Rayne <DiscipleRayne@gmail.com>
2010-04-15 18:02:54 -0500

    Added an example bot.

---
+++ testirclj.clj
@@ -0,0 +1,18 @@
+(use 'irclj.irclj)
+
+(def fnmap {:on-message (fn [{:keys [nick channel message irc]}] 
+			  (let [[cmd & more] (.split message " ")] 
+			    (condp = cmd
+			      "$whoareyou?" (send-action irc channel (:name @irc))
+			      "$setnick" (set-nick irc (first more))
+			      "$setmode" (set-mode irc channel (first more) (second more))
+			      nil)))
+	    :on-quit (fn [{:keys [nick reason irc]}] 
+		       (send-message irc "#irclj" (str nick " quit. His reason was: " reason)))
+	    :on-part (fn [{:keys [nick reason channel irc]}]
+		       (send-message irc channel (str nick " parted. Reason: " reason)))})
+
+(def bot (create-bot {:name "ircljbot" :server "irc.freenode.net" :fnmap fnmap}))
+(def newbot (connect bot :channels ["#()" "#irclj"]))
+(read-line)
+(close newbot)
