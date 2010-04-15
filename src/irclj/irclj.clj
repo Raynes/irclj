d0d1e6fa96310173ce2bee5f606a5fcfcff7aeed
Rayne <DiscipleRayne@gmail.com>
2010-04-15 16:48:57 -0500

    Moved test bot to another file.

--- src/irclj/irclj.clj
+++ src/irclj/irclj.clj
@@ -135,19 +135,3 @@
 							    (.println sockout (str "JOIN " channel))))
 				:else (handle (mess-to-map words) irc))))))
     irc))
-
-(def fnmap {:on-message (fn [{:keys [nick channel message irc]}] 
-			  (let [[cmd & more] (.split message " ")] 
-			    (condp = cmd
-			      "$whoareyou?" (send-action irc channel (:name @irc))
-			      "$setnick" (set-nick irc (first more))
-			      nil)))
-	    :on-quit (fn [{:keys [nick reason irc]}] 
-		       (send-message irc "#irclj" (str nick " quit. His reason was: " reason)))
-	    :on-part (fn [{:keys [nick reason channel irc]}]
-		       (send-message irc channel (str nick " parted. Reason: " reason)))})
-
-(def bot (create-bot {:name "ircljbot" :server "irc.freenode.net" :fnmap fnmap}))
-(def newbot (connect bot :channels ["#irclj"]))
-(read-line)
-(close newbot)
