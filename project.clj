861f7e6b43c17c3b7dd4099638840c3ac3d4ed78
Rayne <DiscipleRayne@gmail.com>
2010-04-15 11:19:15 -0500

    Added Clojure/contrib 1.2.0 dep and fixed an error.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject irclj "0.0.1-SNAPSHOT"
   :description "A simple IRC library/bot framework."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.1.0"]
 		     [leiningen/lein-swank "1.1.0"]
 		     [lein-search "0.3.0-SNAPSHOT"]])
--- src/irclj/irclj.clj
+++ src/irclj/irclj.clj
@@ -2,8 +2,9 @@
     #^{:author "Anthony Simpson (Rayne)",
        :doc "A small IRC library to abstract over lower-level IRC connection handling."} 
     irclj.irclj
-  (:import [java.io PrintStream PrintWriter BufferedReader InputStreamReader]
-	   java.net.Socket))
+    (:use clojure.contrib.io)
+    (:import [java.io PrintStream PrintWriter BufferedReader InputStreamReader]
+	     java.net.Socket))
 
 (defn mess-to-map
   ""
@@ -15,29 +16,52 @@
 		     :doing doing}]
     (merge message-map 
 	   (condp = doing
-	     "PRIVMSG" {:channel (first more) :message (apply str (butlast (rest (second more))))}
+	     "PRIVMSG" {:channel (first more) :message (->> more second rest butlast (apply str))}
 	     "QUIT" {:reason (apply str (rest more))}
 	     "JOIN" {:channel (apply str (rest more))}
-	     "PART" {:channel (first more) :reason (apply str (rest (second more)))}))))
-  
+	     "PART" {:channel (first more) :reason (apply str (rest (second more)))}
+	     "NOTICE" {:target (first more) :message (->> more second rest butlast (apply str))}
+	     "MODE" {:channel (first more) :mode (second more) :user (last more)}
+	     {}))))
+
+(defn handle [& more])
+
+(defn close
+  "Closes an IRC connection."
+  [{:keys [sock sockin sockout]}]
+  (.println sockout "QUIT")
+  (.close sock)
+  (.close sockin)
+  (.close sockout))
+
 (defn connect
   ""
   [{:keys [name password server channels username port realname fnmap] 
     :or {port 6667 username "irclj" realname "I'm a robot, hear me roar!"
-	 channels ["#irclj"]}}]
+	 channels ["#()"]} :as botmap}]
   (let [sock (Socket. server port)
-	sockin (PrintWriter. (.getOutputStream sock) true)
-	sockout (BufferedReader. (InputStreamReader. (.getInputStream sock)))]
+	sockout (PrintWriter. (.getOutputStream sock) true)
+	sockin (reader (input-stream sock))]
     (doto sockout
       (.println (str "NICK " name))
       (.println (str "USER " username " na na :" realname)))
-    (while (not (.isClosed sock))
-	   (let [rline (.readLine sockin)
-		 line (apply str (rest rline))
-		 words (.split lines " ")]
-	     (println line)
-	     (cond
-	      (.startsWith rline "PING") (.println sockout (.replace rline "PING" "PONG"))
-	      (= (nth words 1) "001") (doseq [channel channels] 
-					(.println sockout (str "JOIN " channel))))
-	     ))))
+    (.start (Thread. (fn []
+		       (while (not (.isClosed sock))
+			      (let [rline (try (.readLine sockin) 
+					       (catch java.net.SocketException _ "Socket Closed."))
+				    line (apply str (rest rline))
+				    words (.split line " ")]
+				(println rline)
+				(cond
+				 (.startsWith rline "PING") (.println sockout (.replace rline "PING" "PONG"))
+				 (= (second words) "001") (doseq [channel channels] 
+							    (.println sockout (str "JOIN " channel))))
+				:else (handle (mess-to-map words) fnmap))))))
+    (merge botmap {:sock sock :sockin sockin :sockout sockout})))
+
+
+(def bot {:name "ircljbot" :server "irc.freenode.net"})
+
+(def newbot (connect bot))
+(read-line)
+(close newbot)
