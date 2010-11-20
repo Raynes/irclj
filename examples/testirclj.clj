edefe75aaa182f88112aea4f9d131df42e25a1a5
Raynes <DiscipleRayne@gmail.com>
2010-11-20 15:52:50 -0600

    Fixed a #channel vs channel bug in the JOIN handler.

--- examples/testirclj.clj
+++ examples/testirclj.clj
@@ -1,4 +1,4 @@
-(use 'irclj.irclj)
+(use 'irclj.core)
 
 (def fnmap {:on-message (fn [{:keys [nick channel message irc]}] 
 			  (let [[cmd & more] (.split message " ")] 
@@ -15,18 +15,19 @@
 			      "!topic?" (doseq [x (vals (get-topic irc (first more)))] (println x))
                               "!printirc" (println @irc)
                               "!dividebyzero" (/ 1 0)
+                              "!ping" (send-msg irc "PING" "blahblahblah")
 			      nil)))
 	    :on-quit (fn [{:keys [nick reason irc]}] 
 		       (send-message irc "#irclj" (str nick " quit. His reason was: " reason)))
 	    :on-part (fn [{:keys [nick reason channel irc]}]
 		       (send-message irc channel (str nick " parted. Reason: " reason)))
-	    :on-action (fn [{:keys [nick message reason channel irc]}]
-			 (send-message irc channel (str "you said " message)))
+	    on-action (fn [{:keys [nick message reason channel irc]}]
+                        (send-message irc channel (str "you said " message)))
             :on-connect (fn [_] (println "\n\nON-CONNECT TRIGGERED.\n\n"))
             :on-error (fn [{:keys [irc channel error doing]}]
                         (send-message irc channel (str "While executing the " doing " handler, an error occurred: " (.getMessage error))))})
 
 (def bot (connect (create-irc {:name "ircljbot" :server "irc.freenode.net" :fnmap fnmap}) 
-		  :channels ["#irclj" ["#keyed" "secret"]]))
+		  :channels ["#irclj" "#dld" ["#keyed" "secret"]]))
 (read-line)
 (close bot)
