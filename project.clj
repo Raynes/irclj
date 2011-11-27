6d8fd1ef9e15bd9cdf5ee7d6037c8e479b6d7109
Anthony Grimes <i@raynes.me>
2011-11-27 12:45:27 -0600

    No longer depend on Slingshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject irclj "0.5.0-SNAPSHOT"
   :description "A simple IRC library/bot framework."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [slingshot "0.9.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
