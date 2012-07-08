8b3e1b6f5f1b94f4b9b656d8d981a5b3762d8834
Anthony Grimes <i@raynes.me>
2012-07-08 17:40:03 -0500

    0.5.0-alpha1

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject irclj "0.5.0-SNAPSHOT"
+(defproject irclj "0.5.0-alpha1"
   :description "A simple IRC library/bot framework."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
