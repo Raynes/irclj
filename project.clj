6c7cb7309a11cccda495f2121d9a6ec0836bd56f
Anthony Grimes <i@raynes.me>
2012-07-08 17:41:24 -0500

    Add :url and :license keys.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject irclj "0.5.0-alpha1"
   :description "A simple IRC library/bot framework."
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/flatland/irclj"
   :dependencies [[org.clojure/clojure "1.4.0"]])
