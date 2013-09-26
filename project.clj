6453eab2a85678b4388ea8551f1201ed8a5009db
Anthony Grimes <i@raynes.me>
2013-09-26 10:46:57 -0700

    Inc alpha.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject irclj "0.5.0-alpha2"
+(defproject irclj "0.5.0-alpha3"
   :description "A simple IRC library/bot framework."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
